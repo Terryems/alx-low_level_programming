426658a3c581827adae4ec5f69ce21c6ff91a0bb
Terryems <emmanuelsimona@gmail.com>
2022-09-08 14:09:56 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
+ *  * main - Write 'Programming is like building a multilingual puzzle'
  *   * Return: Always 0 (success)
  *    */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
