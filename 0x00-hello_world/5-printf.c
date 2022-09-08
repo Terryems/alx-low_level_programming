464bbd6441b2cdb4d9381dda0afc4c44efd0283a
Terryems <emmanuelsimona@gmail.com>
2022-09-08 14:15:37 -0700

    n

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programing is like building a multilingual puzzle
-* Return: 0
-*/
+ *  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
+ *   * Return: Always 0 (success)
+ *    */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n);
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
