4f1e29abe154fa15e7d631f82782b292d001450a
Terryems <emmanuelsimona@gmail.com>
2022-09-13 14:54:54 -0700

    else or why

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,18 @@
-#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the stdout
+ * Return: 0 if successful
+ */
 int main(void)
 {
-	printf ("")
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
