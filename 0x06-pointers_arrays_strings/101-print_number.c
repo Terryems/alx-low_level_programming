b65e544313a1fe819bd1ad12029c46d0075bafa9
Terryems <emmanuelsimona@gmail.com>
2022-09-22 20:16:35 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,23 @@
 #include "main.h"
 /**
- * rot13 - function that encode string
- * @c: char variable
- * Return: 0 always
+ * print_number - function that prints an integer
+ * @n: is an integer assigned
+ * Return: nothing
  **/
-char *rot13(char *c)
+void print_number(int n)
 {
-	int i, j;
+	unsigned int i;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; c[i] != '\0'; i++)
+	if (n < 0)
 	{
-		for (j = 0; a[j] != '\0'; j++)
-		{
-			if (c[i] == a[j])
-			{
-				c[i] = b[j];
-				break;
-			}
-		}
+		_putchar('-');
+		i = -n;
 	}
-	return (c);
+	else
+	{
+		i = n;
+	}
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
