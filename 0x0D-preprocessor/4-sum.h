f9353ad13789894ca10bab67d1796e706502a732
Terryems <emmanuelsimona@gmail.com>
2022-10-11 08:20:27 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,12 +1,5 @@
-#ifndef SUM_H
-#define SUM_H
-
-/**
- * define - function that sum two numbers
- * SUM: macro function name
-* Desc: Header file that defines a function-like macro SUM(x, y)
- *       that computes the sum of x and y.
- */
+#ifndef SUM_LIKE_MACRO
+#define SUM_LIKE_MACRO
 
 #define SUM(x, y) ((x) + (y))
 
